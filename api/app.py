070c4f0700efcc195a7b53601c5ced046d41bee4
rose413 <irenechang949@gmail.com>
2025-05-17 12:54:18 -0700

    Add API key and create Flask backend for TripAdvisor API
    - Set up Flask app with initial route(s)
    - Integrated TripAdvisor API using provided API key

---
+++ api/app.py
@@ -0,0 +1,39 @@
+# Flask API backend that provides endpoints to fetch nearby locations and location details from the TripAdvisor API. 
+from dotenv import load_dotenv
+import os
+import requests
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+# Obtain the API key
+load_dotenv()
+api_key = os.getenv("SECRET_KEY")
+
+# Create a Flask application instance
+app = Flask(__name__) 
+
+
+url = f"https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=37.354107%2C%20-121.955238&key={api_key}&language=en"
+headers = {"accept": "application/json"}
+
+# response = requests.get(url, headers=headers)
+@app.route('/api/nearby_search', methods=['GET'])
+def get_nearby_search():
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+    url = f"https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong={latitude}%2C%20{longitude}&key={api_key}&language=en"
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json()), response.status_code
+
+@app.route('/api/location_details', methods=['GET'])
+def get_location_details():
+    location_id = request.args.get('location_id')
+    url = f"https://api.content.tripadvisor.com/api/v1/location/{location_id}/details?key={api_key}&language=en"
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json()), response.status_code
+
+if __name__ == "__main__":
+    CORS(app)
+    app.run(debug=True, host='0.0.0.0', port=3000)
