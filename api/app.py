fb091efc2141bb2ac9f0a8cb3b4ea42d8c09c9da
rose413 <irenechang949@gmail.com>
2025-05-17 14:30:55 -0700

    Updated app.py
    - Added route to handle 'find' search queries
    - Add endpoint to retrieve photos for a location

--- api/app.py
+++ api/app.py
@@ -13,8 +13,8 @@ api_key = os.getenv("SECRET_KEY")
 app = Flask(__name__) 
 
 
-url = f"https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=37.354107%2C%20-121.955238&key={api_key}&language=en"
-headers = {"accept": "application/json"}
+#url = f"https://api.content.tripadvisor.com/api/v1/location/nearby_search?latLong=37.354107%2C%20-121.955238&key={api_key}&language=en"
+# headers = {"accept": "application/json"}
 
 # response = requests.get(url, headers=headers)
 @app.route('/api/nearby_search', methods=['GET'])
@@ -34,6 +34,29 @@ def get_location_details():
     response = requests.get(url, headers=headers)
     return jsonify(response.json()), response.status_code
 
+@app.route('/api/find_search', methods=['GET'])
+def get_find_search():
+    search_query = request.args.get('searchQuery')
+    # address = request.args.get('address')
+    # if address:
+    #     url = f"https://api.content.tripadvisor.com/api/v1/location/find_search?searchQuery={search_query}&address={address}&key={api_key}&language=en"
+    # else:
+    url = f"https://api.content.tripadvisor.com/api/v1/location/search?searchQuery={search_query}&key={api_key}&language=en"
+    headers = {
+        "accept": "application/json",
+        "X-TripAdvisor-API-Key": api_key
+        }
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json()), response.status_code
+
+@app.route('/api/location_photos', methods=['GET'])
+def get_location_photos():
+    location_id = request.args.get('location_id')
+    limit = request.args.get('limit', default=1)
+    url = f"https://api.content.tripadvisor.com/api/v1/location/{location_id}/photos?key={api_key}&language=en&limit={limit}"
+    headers = {"accept": "application/json"}
+    response = requests.get(url, headers=headers)
+    return jsonify(response.json()), response.status_code
 if __name__ == "__main__":
     CORS(app)
     app.run(debug=True, host='0.0.0.0', port=3000)
