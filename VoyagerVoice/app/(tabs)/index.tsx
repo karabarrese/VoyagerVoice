14ffe3d3295f7728dbd13053386ef9695570989f
TiffanyN22 <tnguyen22@ndsj.org>
2025-05-17 11:56:32 -0700

    Created basic intro page

--- VoyagerVoice/app/(tabs)/index.tsx
+++ VoyagerVoice/app/(tabs)/index.tsx
@@ -1,75 +1,65 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Button, Text, StyleSheet, View, TouchableOpacity} from 'react-native';
+import { useFonts, JustAnotherHand_400Regular } from '@expo-google-fonts/just-another-hand';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const [fontsLoaded] = useFonts({
+    JustAnotherHand_400Regular,
+    'JollyLodger': require('../../assets/fonts/JollyLodger-Regular.ttf'),
+  });
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Voyager Voice</Text>
+      <Text style={styles.subheading}>Stories that travel with you</Text>
+      <TouchableOpacity style={styles.button} onPress={() => alert('Button pressed')}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: '#DAF1F1',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  heading: {
+    textAlign: 'left',
+    marginTop: 20,
+    marginLeft: 20,
+    fontSize: 60,
+    color: '#69868A',
+    fontFamily: 'JollyLodger',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
+  subheading: {
+    textAlign: 'left',
+    marginTop: 20,
+    marginLeft: 20,
+    fontSize: 53,
+    color: '#69868A',
+    fontFamily: 'JustAnotherHand_400Regular',
+  },
+  button: {
+    backgroundColor: '#F7C4B4',
+    padding: 10,
     position: 'absolute',
+    left: 25,
+    right: 25,
+    bottom: 50,
+    height: 75,
+    borderRadius: 15,
+    shadowColor: '#FFDDD3',
+    shadowOffset: { width: 5, height: 5 },
+    shadowOpacity: 1,
+    shadowRadius: 3.84,
+  },
+  buttonText: {
+    margin: 'auto',
+    marginTop: 15,
+    fontSize: 35,
+    color: '#69868A',
+    fontFamily: 'JustAnotherHand_400Regular',
   },
 });
