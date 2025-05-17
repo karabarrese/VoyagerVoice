e427420c94cd855d6574eff4511b0304358db910
rose413 <irenechang949@gmail.com>
2025-05-17 15:08:17 -0700

    Added comments and optional query parameters

--- api/app.py
+++ api/app.py
@@ -25,6 +25,7 @@ def get_nearby_search():
     headers = {"accept": "application/json"}
     response = requests.get(url, headers=headers)
     return jsonify(response.json()), response.status_code
+    #http://localhost:3000/api/nearby_search?latitude=37.354107&longitude=-121.955238
 
 @app.route('/api/location_details', methods=['GET'])
 def get_location_details():
@@ -33,21 +34,25 @@ def get_location_details():
     headers = {"accept": "application/json"}
     response = requests.get(url, headers=headers)
     return jsonify(response.json()), response.status_code
+    #http://localhost:3000/api/location_details?location_id=123156
 
 @app.route('/api/find_search', methods=['GET'])
 def get_find_search():
     search_query = request.args.get('searchQuery')
-    # address = request.args.get('address')
-    # if address:
-    #     url = f"https://api.content.tripadvisor.com/api/v1/location/find_search?searchQuery={search_query}&address={address}&key={api_key}&language=en"
-    # else:
-    url = f"https://api.content.tripadvisor.com/api/v1/location/search?searchQuery={search_query}&key={api_key}&language=en"
-    headers = {
-        "accept": "application/json",
-        "X-TripAdvisor-API-Key": api_key
-        }
+    address = request.args.get('address')
+    latitude = request.args.get('latitude')
+    longitude = request.args.get('longitude')
+    # if address and latitude and longitude:
+    #     url = f"https://api.content.tripadvisor.com/api/v1/location/search?searchQuery={search_query}&address={address}&latLong={latitude}%2C%20{longitude}&key={api_key}&language=en"
+    if address: 
+        url = f"https://api.content.tripadvisor.com/api/v1/location/search?searchQuery={search_query}&address={address}&key={api_key}&language=en"
+    else:
+        url = f"https://api.content.tripadvisor.com/api/v1/location/search?searchQuery={search_query}&key={api_key}&language=en"
+    headers = {"accept": "application/json"}
     response = requests.get(url, headers=headers)
     return jsonify(response.json()), response.status_code
+    #http://localhost:3000/api/find_search?searchQuery=UCLA
+    #http://localhost:3000/api/find_search?searchQuery=UCLA&address=850%20Willow%20Creek%20Road%2C%20Lake%20Arrowhead%2C%20CA%2092352
 
 @app.route('/api/location_photos', methods=['GET'])
 def get_location_photos():
@@ -57,6 +62,8 @@ def get_location_photos():
     headers = {"accept": "application/json"}
     response = requests.get(url, headers=headers)
     return jsonify(response.json()), response.status_code
+    #http://localhost:3000/api/location_photos?location_id=80039
+
 if __name__ == "__main__":
     CORS(app)
     app.run(debug=True, host='0.0.0.0', port=3000)
